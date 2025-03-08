eac3d66ebcd6b6b5e84ec2c27aec2e526536f449
loquendo <54327490+FrancoAstorga@users.noreply.github.com>
2025-03-08 17:32:06 -0300

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -12,6 +12,6 @@ fi
 # Upload files
 zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive
 --binary-files "cobol-check-0.2.9.jar"
-# Verify upload :)
+# Verify upload :V
 echo "Verifying upload:"
 zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
